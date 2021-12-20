5a32f997dfe25147d1c4f4cb869804f07d670117
FranklinWposs <franklinortiz@wposs.com>
2021-12-20 16:39:45 -0500

    *Creacion de proyecto

---
+++ app/src/main/java/com/example/pruebafact/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.pruebafact;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pruebafact.conexion.Conexion;
+import com.example.pruebafact.conexion.ConexionPostman;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
